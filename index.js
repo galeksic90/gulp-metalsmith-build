91dfe8a5aa9e32b59feed9eea72d76a1b6b88c7e
macbook <goganogga@gmail.com>
2015-05-12 02:38:53 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+var async = require('async');
+var exec = require('child_process').exec;
+
+
+module.exports.build = function build(cb) {
+
+	return async.series([
+			function(callback) {
+				exec("node node_modules/gulp-metalsmith-build/lib/build.js", callback);
+			},
+			function(callback) {
+				exec("rm -rf public", callback);
+			},
+			function(callback) {
+				exec("cp -r .tmp/ public/", callback);
+			},
+			function(callback) {
+				cb();
+			}
+		],
+		// optional callback
+		function(err, results) {
+			console.log(new Date() + ': ' + err)
+		}
+	);
+
+}
